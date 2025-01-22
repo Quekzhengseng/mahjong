b03ed6ce7f4f6ad30e65249e5b0b673c8fade076
Quekzhengseng <148545414+Quekzhengseng@users.noreply.github.com>
2025-01-23 01:41:23 +0800

    push

---
+++ mahjong-game frontend/src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import CurrentHand from "./components/currenthand";
+
+export default function Home() {
+  return (
+    <div>
+      <CurrentHand></CurrentHand>
+    </div>
+  );
+}
