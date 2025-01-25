9b2cb8203d5bf33900bceff14cbc0c21821fb11d
Quekzhengseng <148545414+Quekzhengseng@users.noreply.github.com>
2025-01-26 01:12:53 +0800

    Gg

--- backend/src/main/java/com/mahjong/config/WebSocketServer.java
+++ backend/src/main/java/com/mahjong/config/WebSocketServer.java
@@ -2,34 +2,56 @@ package com.mahjong.config;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.HandlerWrapper;
 import org.eclipse.jetty.websocket.server.WebSocketHandler;
 import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
 import org.springframework.stereotype.Component;
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 
 @Component
 public class WebSocketServer {
     private final Server server;
+    private final WebSocketHandler wsHandler;
 
     public WebSocketServer() {
+        // Create the server
         server = new Server();
+        
+        // Configure server connector
         ServerConnector connector = new ServerConnector(server);
         connector.setPort(80);
         server.addConnector(connector);
         
-    WebSocketHandler wsHandler = new WebSocketHandler() {
-        @Override
-        public void configure(WebSocketServletFactory factory) {
-            factory.register(GameWebSocketHandler.class);
-            factory.getPolicy().setIdleTimeout(10000);
-        }
-    };
+        // Create WebSocket handler
+        wsHandler = new WebSocketHandler() {
+            @Override
+            public void configure(WebSocketServletFactory factory) {
+                factory.register(GameWebSocketHandler.class);
+                factory.getPolicy().setIdleTimeout(10000);
+            }
+        };
         
-        server.setHandler(wsHandler);
+        // Wrap WebSocket handler to be compatible with Server.setHandler()
+        HandlerWrapper handlerWrapper = new HandlerWrapper() {
+            @Override
+            protected void doStart() throws Exception {
+                super.doStart();
+                setHandler(wsHandler);
+            }
+        };
+        
+        // Set the handler wrapper on the server
+        server.setHandler(handlerWrapper);
     }
 
     @PostConstruct
     public void start() throws Exception {
         server.start();
+    }
+
+    @PreDestroy
+    public void stop() throws Exception {
+        server.stop();
     }
 }
